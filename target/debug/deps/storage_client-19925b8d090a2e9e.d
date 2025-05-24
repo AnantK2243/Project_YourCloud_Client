917165cbe96502509fbca22eb63bf69dfcd33ba4
Anant Kumar <anantone72@gmail.com>
2025-05-24 11:50:23 -0400

    Update structure

---
+++ target/debug/deps/storage_client-19925b8d090a2e9e.d
@@ -0,0 +1,9 @@
+/home/anantk/Documents/project_unknown/storage_client/target/debug/deps/libstorage_client-19925b8d090a2e9e.rmeta: src/main.rs src/config.rs src/network.rs src/storage.rs src/commands.rs
+
+/home/anantk/Documents/project_unknown/storage_client/target/debug/deps/storage_client-19925b8d090a2e9e.d: src/main.rs src/config.rs src/network.rs src/storage.rs src/commands.rs
+
+src/main.rs:
+src/config.rs:
+src/network.rs:
+src/storage.rs:
+src/commands.rs:
---
+++ target/debug/deps/storage_client-54d6b81db79235e2.d
@@ -0,0 +1,9 @@
+/home/anantk/Documents/project_unknown/storage_client/target/debug/deps/storage_client-54d6b81db79235e2: src/main.rs src/config.rs src/network.rs src/storage.rs src/commands.rs
+
+/home/anantk/Documents/project_unknown/storage_client/target/debug/deps/storage_client-54d6b81db79235e2.d: src/main.rs src/config.rs src/network.rs src/storage.rs src/commands.rs
+
+src/main.rs:
+src/config.rs:
+src/network.rs:
+src/storage.rs:
+src/commands.rs:
